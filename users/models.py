9f901697d45fa512e497cf3fd9343afb748f82bd
abduqodir <ttheonepersonn@gmail.com>
2026-01-31 18:55:30 +0500

    customusermodel

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class CustomUser(models.Model,AbstractUser):
+    ROLE_CHOICES=[
+        ('dev','Dev'),
+        ('viewer','Viewer')
+        
+    ]
+    role =  models.CharField(choices=ROLE_CHOICES,default='dev')
+    
+    def __str__(self):
+        return f"{self.username},{self.role}"
