cca8858f1f6763151d46832a94b8fc4de2439197
abduqodir <ttheonepersonn@gmail.com>
2026-01-31 20:10:39 +0500

    codefile model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+
+class CodeFile(models.Model):
+    title= models.CharField(max_length=50,default='no title')
+    file=models.FileField(upload_to='/files')
+    dev=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.title}, {self.dev}"
